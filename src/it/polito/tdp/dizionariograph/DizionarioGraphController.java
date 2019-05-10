6316798e8431b79d806d0f02158bbba7f9513e31
Silvio Senese <s228789@studenti.polito.it>
2019-05-10 18:56:38 +0200

    Implementata funzione displayNeighbors + qualche controllo sugli input

--- src/it/polito/tdp/dizionariograph/DizionarioGraphController.java
+++ src/it/polito/tdp/dizionariograph/DizionarioGraphController.java
@@ -5,11 +5,13 @@ import java.util.*;
 import it.polito.tdp.dizionariograph.model.Model;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class DizionarioGraphController {
 
+	private int numeroLettere=0;
     @FXML
     private ResourceBundle resources;
 
@@ -25,19 +27,65 @@ public class DizionarioGraphController {
     @FXML
     private TextArea txtResult;
 
+    @FXML
+    private Button btnGeneraGrafo;
+
+    @FXML
+    private Button btnTrovaVicini;
+
+    @FXML
+    private Button btnGradoMax;
+
 	private Model model;
 
     @FXML
     void doGeneraGrafo(ActionEvent event) {
-    	//Leggo il valore inserito nella prima casella di testo
-    	int numeroLettere=Integer.parseInt(txtNumeroLettere.getText());
+    	//Controllo se la prima casella è vuota
+    	if(txtNumeroLettere.getText().isEmpty()) {
+    		txtResult.appendText("Errore: inserire un numero intero nel primo campo di testo");
+    		return;
+    	}
+    	
+    	//Leggo il valore inserito nella prima casella di testo, ma devo controllare se si verifica un errore di conversione da stringa a intero
+    try{
+    	
+    	if(numeroLettere!=Integer.parseInt(txtNumeroLettere.getText())) {
+    		numeroLettere=Integer.parseInt(txtNumeroLettere.getText());
+    	}
+    	else {//Controllo utile solo per alleggerire i calcoli richiesti al programma, ma  inutile per il resto
+    		txtResult.appendText("Grafo della dimensione richiesta già creato\n");
+    		return;
+    	}
     	
     	//Ora devo chiamare il model affinche' generi il grafo non orientato
     	model.createGraph(numeroLettere);
+    	txtResult.appendText("Grafo creato\n");
+    	
+    	//Attivo il pulsante successivo
+    	btnTrovaVicini.setDisable(false);
+    	//E nel caso in cui sia acceso il terzo pulsante lo devo disattivare
+    	btnGradoMax.setDisable(true);
+    }
+    	catch(NumberFormatException n) {
+    		System.err.println("Errore nella conversione in intero del valore inserito nella prima casella di testo");
+    	}
+    	
     }
 
     @FXML
     void doTrovaVicini(ActionEvent event) {
+    	//Errori
+    	if(txtParolaDaCercare.getText().trim().isEmpty()) {
+    		txtResult.appendText("Errore: inserire una parola nel secondo campo  di testo\n");
+    		return;
+    	}
+    	
+    	System.out.println(txtParolaDaCercare.getText().trim().length()+"   "+Integer.parseInt(txtNumeroLettere.getText()));
+    	if(txtParolaDaCercare.getText().trim().length()!=Integer.parseInt(txtNumeroLettere.getText())) {
+    		txtResult.appendText("Errore: inserire una parola della lunghezza corretta\n");
+    		return;
+    	}
+    	
     	List<String> figli= new ArrayList<String>();
     	//Il grafo deve essere gia' creato
     	
@@ -45,25 +93,40 @@ public class DizionarioGraphController {
     	String parolaInserita = txtParolaDaCercare.getText();
     	
     	//Chiamo il model affinche' trovi i figli della parola inserita e me li salvi in una lista
-    	txtResult.setText("Le parole che differiscono per una sola lettera da "+parolaInserita.toUpperCase()+ " sono:\n");
     	figli = model.displayNeighbours(parolaInserita);
-    	for(String s: figli) {
-    		txtResult.setText(s+ "\n");
+    	if(figli.size()==0) {
+    		//La parola cercata non e' presente nel database
+    		txtResult.appendText(parolaInserita.toUpperCase()+ " non è presente nel database. RIPROVA\n");
+    		return;
     	}
+    	
+    	txtResult.appendText("Le parole che differiscono per una sola lettera da "+parolaInserita.toUpperCase()+ " sono "+figli.size()+":\n");
+    	for(String s: figli) {
+    		txtResult.appendText(s+ "\n");
+    	}
+    	
+    	//Attivo il pulsante successivo
+    	btnGradoMax.setDisable(false);
+    	
     }
     
     @FXML
     void doTrovaGradoMax(ActionEvent event) {
-
+    	
     }
 
 
 
     @FXML
     void doReset(ActionEvent event) {
+    	//Pulisco la vista
     	txtNumeroLettere.clear();
     	txtParolaDaCercare.clear();
     	txtResult.clear();
+    	
+    	//Oscuro i bottoni 2 e 3
+    	btnTrovaVicini.setDisable(true);
+    	btnGradoMax.setDisable(true);
     }
     
     @FXML
@@ -71,7 +134,9 @@ public class DizionarioGraphController {
         assert txtNumeroLettere != null : "fx:id=\"txtNumeroLettere\" was not injected: check your FXML file 'DizionarioGraph.fxml'.";
         assert txtParolaDaCercare != null : "fx:id=\"txtParolaDaCercare\" was not injected: check your FXML file 'DizionarioGraph.fxml'.";
         assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'DizionarioGraph.fxml'.";
-
+        assert btnGeneraGrafo != null : "fx:id=\"btnGeneraGrafo\" was not injected: check your FXML file 'DizionarioGraph.fxml'.";
+        assert btnTrovaVicini != null : "fx:id=\"btnTrovaVicini\" was not injected: check your FXML file 'DizionarioGraph.fxml'.";
+        assert btnGradoMax != null : "fx:id=\"btnGradoMax\" was not injected: check your FXML file 'DizionarioGraph.fxml'.";
     }
 
 
--- src/it/polito/tdp/dizionariograph/model/Model.java
+++ src/it/polito/tdp/dizionariograph/model/Model.java
@@ -3,54 +3,140 @@ package it.polito.tdp.dizionariograph.model;
 import java.util.*;
 import org.jgrapht.Graph;
 import org.jgrapht.Graphs;
+import org.jgrapht.event.ConnectedComponentTraversalEvent;
+import org.jgrapht.event.EdgeTraversalEvent;
+import org.jgrapht.event.TraversalListener;
+import org.jgrapht.event.VertexTraversalEvent;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.SimpleGraph;
+import org.jgrapht.traverse.BreadthFirstIterator;
+import org.jgrapht.traverse.GraphIterator;
 
 import it.polito.tdp.dizionariograph.db.WordDAO;
 
 public class Model {
+	
+	private class EdgeTraversedGraphListener implements TraversalListener<String, DefaultEdge> {
+
+		@Override
+		public void connectedComponentFinished(ConnectedComponentTraversalEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void connectedComponentStarted(ConnectedComponentTraversalEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void edgeTraversed(EdgeTraversalEvent<DefaultEdge> ev) {
+			// TODO Auto-generated method stub
+			//Ogni volta che ha visitato un arco, deve capire se il nodo figlio visitato e' nuovo o no.
+			//Se e' la prima volta che viene visitato allora viene salvato nella mappa back con chiva=nodo figlio e argomento nodo padre
+			//back e' una mappa che punta dal basso vero l'alto (child->parent)
+			//per un nuovo vertice 'child' scoperto devo avere che:
+			// - child e' ancora conosciuto(non ancora trovato)
+			// - parent gia' visitato
+			
+			String sourceVertex = grafo.getEdgeSource(ev.getEdge());
+			String targetVertex = grafo.getEdgeTarget(ev.getEdge());
+
+			/*
+			 * se il grafo e' orientato allora source=parent, target==child
+			 * se non e' orientato, potrebbe essere al contrario
+			 */
+			
+			//se questo target non e' ancora una chiave della mappa e intanto il source e' gia' nella mappa(vuol dire che so da dove proviene)
+			if(!backVisit.containsKey(targetVertex) && backVisit.containsValue(sourceVertex)) {
+				backVisit.put(targetVertex, sourceVertex);
+			} else if(!backVisit.containsKey(sourceVertex) && backVisit.containsValue(targetVertex)){//questo serve solo nel caso di grafi non orientati
+				backVisit.put(sourceVertex, targetVertex);
+			}
+						
+		}
+
+		@Override
+		public void vertexFinished(VertexTraversalEvent<String> arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void vertexTraversed(VertexTraversalEvent<String> arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+	
+		
+	}
 	private Graph <String, DefaultEdge> grafo;
 	private List<String> parole;
 //	private Map<Integer, String> wordIdMap;
-	
+	private Map<String, String> backVisit;//albero all'indietro relativo alla visita
 	
 	
 	public void createGraph(int numeroLettere) {
 		//Grafo non orientato non pesato---->SimpleGraph
-		this.grafo = new SimpleGraph<>(DefaultEdge.class);
+		this.grafo = new SimpleGraph<String, DefaultEdge>(DefaultEdge.class);
 
 		//Adesso per aggiungere i vertici al grafo ho bisogno del db dizionario, quindi chiamo il DAO
 		WordDAO dao = new WordDAO();
-		this.parole=dao.getAllWordsFixedLength(numeroLettere);
-				
+		parole=new ArrayList<String>(dao.getAllWordsFixedLength(numeroLettere));
+		
+		if (parole.size() == 0) {
+			System.out.println("Nessuna parola trovata");
+			return;
+}
 		//Qui aggiungo al grafo creato in precedenza le parole della lunghezza specificata
 		Graphs.addAllVertices(this.grafo, this.parole);
 		
 		//Adesso devo aggiungere gli archi
 		//Un arco puo' collegare solo le parole che differiscono di una sola lettera
-		for(String s1: this.parole) {
+		for(String s1: parole) {
 			//Prendo una parola e controllo con quali  parole differisce di una sola lettera
 			//e per ognuna di queste parole creo un arco
 			
-			for(String s2: this.parole) {
+			for(String s2: parole) {
 				//Qui ho s1 fissa e la confronto con tutte le altre parole di lunghezza specificata
-				if(controllaParole(s1, s2)) {
+				if(controllaParole(s1, s2) && s1.compareTo(s2)!=0) {
 					this.grafo.addEdge(s1, s2);
 				}
 			}//uscito da questo for comincera' dalla seconda parola e la confronta con tutte quelle successive
 			//Questo si puo' fare perche' il grafo e' non orientato e quindi se due parole differiscono di una sola lettera verra' creato l'edge e non ci sara' piu' bisogno di confrontarle
 		}
 		
-		
 		System.err.println("createGraph -- TODO");
+		System.out.println("Vertici: "+this.grafo.vertexSet().size()+" Archi: "+this.grafo.edgeSet().size());
+		
 	}
 
 	
 	//Per fare questo serve la lezione 26
 	public List<String> displayNeighbours(String parolaInserita) {
-
+		List<String> result = new ArrayList<String>();
+		if (!parole.contains(parolaInserita)) {
+			return result;
+		}
+		result=Graphs.neighborListOf(this.grafo, parolaInserita);
 		System.err.println("displayNeighbours -- TODO");
-		return new ArrayList<String>();
+		return result;
+		
+//		backVisit=new HashMap<String, String>();
+//		
+//		GraphIterator<String, DefaultEdge> it = new BreadthFirstIterator<>(this.grafo, parolaInserita);
+//		
+//		it.addTraversalListener(new Model.EdgeTraversedGraphListener());
+//		
+//		backVisit.put(parolaInserita, null);
+//		
+//		while(it.hasNext()) {
+//			result.add(it.next());
+//		}
+//		
+		
+//		return result;
 	}
 
 	public int findMaxDegree() {
@@ -64,21 +150,16 @@ public class Model {
 		
 		//Fisso una lettera della prima parola e...
 		for(int i=0; i<s1.length(); i++) {
-			
-			//scorro tutte le lettere della seconda
-			for(int j=0; j<s1.length(); j++) {
-				
-				//ogni volta che trovo una differenza aumento il contatore
-				if(s1.charAt(i)!=s2.charAt(j)) {
-					contatore++;
-					//se il contatore supera 1, allora posso ritornare false e non far lavorare piu' il programma
-					if(contatore>1)
-						return false;
-				}
+			//ogni volta che trovo una differenza aumento il contatore
+			if(s1.charAt(i)!=s2.charAt(i)) {
+				contatore++;
+				//se il contatore supera 1, allora posso ritornare false e non far lavorare piu' il programma
+				if(contatore>1) {
+					break;}
 			}
 		}
 		
-		if(contatore == 1)
+		if(contatore <= 1)
 			return true;
 		else
 			return false;
